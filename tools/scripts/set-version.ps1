1e8263792b482d0b1b4d769dd3730e4d36c4c0c3
Hans Gschossmann <johanng@microsoft.com>
2020-05-11 13:21:09 +0200

    update to latest build scripts

--- tools/scripts/set-version.ps1
+++ tools/scripts/set-version.ps1
@@ -9,9 +9,14 @@
 
 $version = & (Join-Path $PSScriptRoot "get-version.ps1")
 
+# Call versioning for build
+& nbgv  @("cloud", "-c", "-a")
+if ($LastExitCode -ne 0) {
+   Write-Warning "Error: 'nbgv cloud -c -a' failed with $($LastExitCode)."
+}
+
 # Set build environment version numbers in pipeline context
 Write-Host "Setting version build variables:"
-Write-Host "Version full: $($version.Full)"
-Write-Host "Version prefix: $($version.Prefix)"
+
 Write-Host "##vso[task.setvariable variable=Version_Full;isOutput=true]$($version.Full)"
 Write-Host "##vso[task.setvariable variable=Version_Prefix;isOutput=true]$($version.Prefix)"
