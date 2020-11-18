574ebdaa74ad74a6c1186556ac51da76e044023b
Luis Cantero <lcantero@microsoft.com>
2020-11-18 11:50:35 +0100

    Use supported model version 1.04

--- src/Boiler/BuildDesign.bat
+++ src/Boiler/BuildDesign.bat
@@ -2,6 +2,6 @@
 
 echo Building ModelDesign
 
-docker run --mount type=bind,source=%CD%,target=/model sailavid/ua-modelcompiler -- -console -d2 /model/ModelDesign.xml -cg /model/ModelDesign.csv -o2 /model
+docker run --mount type=bind,source=%CD%,target=/model sailavid/ua-modelcompiler -- -console -d2 /model/ModelDesign.xml -cg /model/ModelDesign.csv -o2 /model -version v104
 
 echo Success!
