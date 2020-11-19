a2a0e161d22550c02a80d91b1fd1fa7b676fbeef
Martin Regen <mregen@microsoft.com>
2020-11-19 18:27:20 +0100

    Add DataTypeDefinition for complex types (V1.04) (#30)
    * Create the modelcompiler output to support DataTypeDefinitions
    
    * reduce number of UA assemblies used
    
    Co-authored-by: Hans Gschossmann <hansgschossmann@users.noreply.github.com>

--- src/Boiler/BuildDesign.bat
+++ src/Boiler/BuildDesign.bat
@@ -2,6 +2,11 @@
 
 echo Building ModelDesign
 
-docker run --mount type=bind,source=%CD%,target=/model sailavid/ua-modelcompiler -- -console -d2 /model/ModelDesign.xml -cg /model/ModelDesign.csv -o2 /model -version v104
+REM Issue> The ModelCompiler from docker doesn't create the DataTypeDefinition'
+REM docker run --mount type=bind,source=%CD%,target=/model sailavid/ua-modelcompiler -- -console -version v104 -d2 /model/ModelDesign.xml -cg /model/ModelDesign.csv -o2 /model
+
+REM Build master branch in https://github.com/OPCFoundation/UA-ModelCompiler
+REM add ModelCompiler build output folder to PATH
+Opc.Ua.ModelCompiler.exe -version v104 -d2 ".\ModelDesign.xml" -c ".\ModelDesign.csv" -o2 ".\"
 
 echo Success!
