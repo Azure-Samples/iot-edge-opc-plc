8b2209df38d38e90de2efdf08fd7ed0f59da61c7
Luis Cantero <lcantero@microsoft.com>
2021-07-29 11:56:56 +0200

    Add unit test

---
+++ tests/GuidNodesTests.cs
@@ -0,0 +1,30 @@
+﻿namespace OpcPlc.Tests
+{
+    using System.Linq;
+    using FluentAssertions;
+    using NUnit.Framework;
+    using Opc.Ua;
+    using static System.TimeSpan;
+
+    /// <summary>
+    /// Tests for OPC-UA Monitoring for Data changes.
+    /// </summary>
+    [TestFixture]
+    public class GuidNodesTests : SubscriptionTestsBase
+    {
+        // Set any cmd params needed for the plc server explicitly
+        public GuidNodesTests() : base(new[] { "--gn=2" })
+        {
+        }
+
+        [Test]
+        public void CreateMonitoredItem()
+        {
+            var guidNode1 = GetOpcPlcNodeId("65e451f1-56f1-ce84-a44f-6addf176beaf");
+            guidNode1.Should().NotBeNull();
+
+            var guidNode2 = GetOpcPlcNodeId("9513141f-c697-8a1f-a236-e14864e4bf7e");
+            guidNode2.Should().NotBeNull();
+        }
+    }
+}
