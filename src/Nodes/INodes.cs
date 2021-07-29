1080646ad4b94aaac7f71a5f51046a4fd0ed8ec6
Luis Cantero <lcantero@microsoft.com>
2021-07-29 13:51:50 +0200

    Add nodes interface and deterministic guid implementation

---
+++ src/Nodes/INodes.cs
@@ -0,0 +1,16 @@
+﻿using Opc.Ua;
+using System;
+
+namespace OpcPlc.Nodes
+{
+    public interface INodes<T>
+    {
+        string Prototype { get; set; }
+        string Description { get; set; }
+        Action<T> Action { get; set; }
+
+        void AddToAddressSpace(FolderState parentFolder, PlcNodeManager plcNodeManager);
+        void StartSimulation(PlcServer server);
+        void StopSimulation();
+    }
+}
