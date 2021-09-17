487c2b3f9a54635d91d2403c499eed65c2509c07
Luis Cantero <lcantero@microsoft.com>
2021-09-17 10:52:15 +0200

    Fix typos

--- src/Namespaces.cs
+++ src/Namespaces.cs
@@ -6,15 +6,15 @@ namespace OpcPlc
     public static partial class Namespaces
     {
         /// <summary>
-        /// The namespace for the nodes provided by for boyler type.
+        /// The namespace for the nodes provided by for boiler type.
         /// </summary>
         public const string OpcPlcBoiler = "http://microsoft.com/Opc/OpcPlc/Boiler";
-        
+
         /// <summary>
-        /// The namespace for the nodes provided by the for the boyler instance.
+        /// The namespace for the nodes provided by the for the boiler instance.
         /// </summary>
         public const string OpcPlcBoilerInstance = "http://microsoft.com/Opc/OpcPlc/BoilerInstance";
-        
+
         /// <summary>
         /// The namespace for the nodes provided by the plc server.
         /// </summary>
