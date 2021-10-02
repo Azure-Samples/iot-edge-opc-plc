6ee83036770a6aed84cfc20974aab5d02343d930
Luis Cantero <lcantero@microsoft.com>
2021-10-02 11:25:21 +0200

    Revert "Test show exception"
    This reverts commit ff804b73b63095f619ffc9a97dfbca277c777d3d.

--- tests/PlcSimulatorFixture.cs
+++ tests/PlcSimulatorFixture.cs
@@ -293,7 +293,7 @@ namespace OpcPlc.Tests
 
                 if (_serverTask.IsCompleted)
                 {
-                    throw new Exception("Server failed to start: " + _serverTask?.Exception?.Flatten().Message);
+                    throw new Exception("Server failed to start");
                 }
 
                 if (!Program.Ready)
