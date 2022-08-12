4310e0c481639d4407b746b31564dbf7025babba
Luis Cantero <lcantero@microsoft.com>
2022-08-12 18:28:07 +0200

    Update DeterministicGuid to generate UUID v4 (#164)
    * Update deterministic guid to generate uuid v4
    
    * Update comment

--- src/Helpers/DeterministicGuid.cs
+++ src/Helpers/DeterministicGuid.cs
@@ -8,18 +8,18 @@ public static class DeterministicGuid
 
     public static Guid NewGuid()
     {
-        return new Guid($"{GetRandHex(4)}-{GetRandHex(2)}-{GetRandHex(2)}-{GetRandHex(2)}-{GetRandHex(6)}");
+        // https://en.wikipedia.org/wiki/Universally_unique_identifier#Format
+        // xxxxxxxx-xxxx-4xxx-[8|9|a|b]xxx-xxxxxxxxxxxx
+        return new Guid($"{GetRandHexExp(0, 16, 8)}-{GetRandHexExp(0, 16, 4)}-{GetRandHex(16_384, 20_479, 4)}-{GetRandHex(32_768, 49_151, 4)}-{GetRandHexExp(0, 16, 12)}");
     }
 
-    private static object GetRandHex(int length)
+    private static string GetRandHexExp(long minIncl, int maxExclBase, int maxExclExponent)
     {
-        string hexString = string.Empty;
+        return GetRandHex(minIncl, (long)Math.Pow(maxExclBase, maxExclExponent), maxExclExponent);
+    }
 
-        for (int i = 0; i < length; i++)
-        {
-            hexString += $"{_rnd.Next(0, 255):x2}";
-        }
-
-        return hexString;
+    private static string GetRandHex(long minIncl, long maxExcl, int digits)
+    {
+        return string.Format($"{{0:x{digits}}}", _rnd.NextInt64(minIncl, maxExcl));
     }
 }
