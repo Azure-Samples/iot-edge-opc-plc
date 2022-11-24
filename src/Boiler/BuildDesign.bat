7dead08042626bbfd7f59c99b36cb23909da2e64
Luis Cantero <lcantero@microsoft.com>
2022-11-24 16:43:41 +0100

    Cosmetic

--- src/Boiler/BuildDesign.bat
+++ src/Boiler/BuildDesign.bat
@@ -3,19 +3,19 @@ setlocal
 
 REM if docker is not available, ensure the Opc.Ua.ModelCompiler.exe is in the PATH
 set MODELCOMPILER=Opc.Ua.ModelCompiler.exe
-REM 
+REM
 set MODELCOMPILERIMAGE=ghcr.io/opcfoundation/ua-modelcompiler:latest
 set MODELROOT=.
 
-echo pull latest modelcompiler from github container registry
+echo Pulling latest ModelCompiler from github container registry ...
 docker pull %MODELCOMPILERIMAGE%
 IF ERRORLEVEL 1 (
 :nodocker
-    Echo The docker command to download ModelCompiler failed. Using local PATH instead to execute ModelCompiler.
+    Echo The docker command to download ModelCompiler failed. Using local PATH instead to execute it.
 ) ELSE (
-    Echo Successfully pulled the latest docker container for ModelCompiler.
+    Echo Successfully pulled the latest docker container for ModelCompiler
     set MODELROOT=/model
-    set MODELCOMPILER=docker run -v "%CD%:/model" -it --rm --name ua-modelcompiler %MODELCOMPILERIMAGE% 
+    set MODELCOMPILER=docker run -v "%CD%:/model" -it --rm --name ua-modelcompiler %MODELCOMPILERIMAGE%
 )
 
 echo Building ModelDesign
