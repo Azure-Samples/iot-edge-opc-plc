f065a716bce886dc6ad426cd4eff88b036f93ad2
Luis Cantero <lcantero@microsoft.com>
2023-02-14 11:49:11 +0100

    Update model designer batch files

--- src/Boiler/ModelCompiler.cmd
+++ src/Boiler/ModelCompiler.cmd
@@ -1,5 +1,6 @@
 @echo off
 setlocal
+set modelName=%1
 
 REM If docker is not available, ensure that Opc.Ua.ModelCompiler.exe is in the PATH environment variable
 set MODELCOMPILER=Opc.Ua.ModelCompiler.exe
@@ -10,17 +11,20 @@ echo Pulling latest ModelCompiler from the GitHub container registry ...
 docker pull %MODELCOMPILERIMAGE%
 IF ERRORLEVEL 1 (
 :nodocker
-    Echo The docker command to download ModelCompiler failed. Using local PATH instead.
+    echo The docker command to download ModelCompiler failed, using local PATH instead
 ) ELSE (
-    Echo Successfully pulled the latest docker container for ModelCompiler
+    echo Successfully pulled the latest docker container for ModelCompiler
     set MODELROOT=/model
     set MODELCOMPILER=docker run -v "%CD%:/model" -it --rm --name ua-modelcompiler %MODELCOMPILERIMAGE%
 )
 
-echo Building ModelDesign ...
-%MODELCOMPILER% compile -version v104 -d2 "%MODELROOT%/ModelDesign.xml" -cg "%MODELROOT%/ModelDesign.csv" -o2 "%MODELROOT%/"
+echo:
+echo Building %modelName%.xml ...
+%MODELCOMPILER% compile -version v104 -d2 "%MODELROOT%/%modelName%.xml" -cg "%MODELROOT%/%modelName%.csv" -o2 "%MODELROOT%/"
+
+echo:
 IF ERRORLEVEL 1 (
-    echo Modelcompiler failed!
+    echo ModelCompiler failed!
 ) ELSE (
-    echo Success!
+    echo ModelCompiler succeeded!
 )
