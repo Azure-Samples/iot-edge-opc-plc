f79912d6589c906c063aaea2efe38b269dcd7464
Martin Regen <mregen@microsoft.com>
2023-05-16 16:42:49 +0200

    use private docker image

--- src/ModelCompiler.cmd
+++ src/ModelCompiler.cmd
@@ -10,6 +10,8 @@ set prefix2=%6
 REM If docker is not available, ensure that Opc.Ua.ModelCompiler.exe is in the PATH environment variable
 set MODELCOMPILER=Opc.Ua.ModelCompiler.exe
 set MODELCOMPILERIMAGE=ghcr.io/opcfoundation/ua-modelcompiler:latest
+REM private build until the official image is updated
+set MODELCOMPILERIMAGE=ghcr.io/mregen/ua-modelcompiler:latest-docker-nodeset2
 set MODELROOT=.
 
 echo Pulling latest ModelCompiler from the GitHub container registry ...
@@ -23,19 +25,37 @@ IF ERRORLEVEL 1 (
     set MODELCOMPILER=docker run -v "%CD%:/model" -it --rm --name ua-modelcompiler %MODELCOMPILERIMAGE%
 )
 
-echo:
+rem filename1
+for %%A in ("%modelName1%") do (
+    set "filename1=%%~nxA"
+)
+rem filename2
+for %%A in ("%modelName2%") do (
+    set "filename2=%%~nxA"
+)
+
+rem Display the results
+echo Model1 Filename : %filename1%
+echo Model2 Filename : %filename2%
+mkdir temp
+
 IF "%2" == "" (
     echo Building %modelName1%.xml ...
     %MODELCOMPILER% compile -version v104 -d2 "%MODELROOT%/%modelName1%.xml" -cg "%MODELROOT%/%modelName1%.csv" -o2 "%MODELROOT%/"
 ) ELSE IF "%4" == "" (
-    echo Building %modelName1%.xml,%namespace1%,%prefix1% ...
-    %MODELCOMPILER% compile -version v104 -id 1000 -d2 "%MODELROOT%/%modelName1%.xml,%namespace1%,%prefix1%" -o2 "%MODELROOT%/"
+    echo Building Nodeset2 %modelName1%.xml,%namespace1%,%prefix1% ...
+    COPY %modelName1%.xml temp
+    %MODELCOMPILER% compile -version v104 -id 1000 -d2 "%MODELROOT%/temp/%filename1%.xml,%namespace1%,%prefix1%" -o2 "%MODELROOT%/"
     IF %ERRORLEVEL% EQU 0 echo Success!
 ) ELSE (
-    echo Building %modelName1%.xml,%namespace1%,%prefix1% %modelName2%.xml,%namespace2%,%prefix2%...
-    %MODELCOMPILER% compile -version v104 -d2 "%MODELROOT%/%modelName1%.xml,%namespace1%,%prefix1%" -d2 "%MODELROOT%/%modelName2%.xml,%namespace2%,%prefix2%" -cg "%MODELROOT%/%modelName1%.csv" -o2 "%MODELROOT%/"
+    COPY %modelName1%.xml temp
+    COPY %modelName2%.xml temp
+    echo Building Nodeset2 %modelName1%.xml,%namespace1%,%prefix1% %modelName2%.xml,%namespace2%,%prefix2%...
+    %MODELCOMPILER% compile -version v104 -d2 "%MODELROOT%/temp/%filename1%.xml,%namespace1%,%prefix1%" -d2 "%MODELROOT%/temp/%filename2%.xml,%namespace2%,%prefix2%" -cg "%MODELROOT%/%modelName1%.csv" -o2 "%MODELROOT%/"
     IF %ERRORLEVEL% EQU 0 echo Success!
 )
+
+rmdir /s/q temp
 
 echo:
 IF ERRORLEVEL 1 (
