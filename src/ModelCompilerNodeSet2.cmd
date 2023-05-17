af53a6835a83d718c4ffecdf84f0c406f606094d
Daniele Colonna <dacol@microsoft.com>
2023-05-17 16:53:00 +0200

    Add node definition for Boiler that inherits from DI (#245)
    * start adding boiler 2
    
    * DI namespace
    
    * add boiler
    
    * project file
    
    * use private docker image
    
    * add pluginNodes for DI and boiler2
    
    * small fix
    
    * fix pr comments
    
    * small fixes
    
    * removed generation of .csv files
    
    ---------
    
    Co-authored-by: Martin Regen <mregen@microsoft.com>

---
+++ src/ModelCompilerNodeSet2.cmd
@@ -0,0 +1,60 @@
+﻿@echo off
+setlocal
+set modelName1=%1
+set namespace1=%2
+set prefix1=%3
+set modelName2=%4
+set namespace2=%5
+set prefix2=%6
+
+REM If docker is not available, ensure that Opc.Ua.ModelCompiler.exe is in the PATH environment variable
+set MODELCOMPILER=Opc.Ua.ModelCompiler.exe
+set MODELCOMPILERIMAGE=ghcr.io/opcfoundation/ua-modelcompiler:latest
+REM private build until the official image is updated
+set MODELCOMPILERIMAGE=ghcr.io/mregen/ua-modelcompiler:latest-docker-nodeset2
+set MODELROOT=.
+
+echo Pulling latest ModelCompiler from the GitHub container registry ...
+docker pull %MODELCOMPILERIMAGE%
+IF ERRORLEVEL 1 (
+:nodocker
+    echo The docker command to download ModelCompiler failed, using local PATH instead
+) ELSE (
+    echo Successfully pulled the latest docker container for ModelCompiler
+    set MODELROOT=/model
+    set MODELCOMPILER=docker run -v "%CD%:/model" -it --rm --name ua-modelcompiler %MODELCOMPILERIMAGE%
+)
+
+rem filename1
+for %%A in ("%modelName1%") do (
+    set "filename1=%%~nxA"
+)
+rem filename2
+for %%A in ("%modelName2%") do (
+    set "filename2=%%~nxA"
+)
+
+rem Display the results
+echo Model1 Filename : %filename1%
+echo Model2 Filename : %filename2%
+mkdir temp
+
+IF "%4" == "" (
+    echo Building Nodeset2 %modelName1%.xml,%namespace1%,%prefix1% ...
+    COPY %modelName1%.xml temp
+    %MODELCOMPILER% compile -version v104 -id 1000 -d2 "%MODELROOT%/temp/%filename1%.xml,%namespace1%,%prefix1%" -o2 "%MODELROOT%/"
+) ELSE (
+    COPY %modelName1%.xml temp
+    COPY %modelName2%.xml temp
+    echo Building Nodeset2 %modelName1%.xml,%namespace1%,%prefix1% %modelName2%.xml,%namespace2%,%prefix2%...
+    %MODELCOMPILER% compile -version v104 -d2 "%MODELROOT%/temp/%filename1%.xml,%namespace1%,%prefix1%" -d2 "%MODELROOT%/temp/%filename2%.xml,%namespace2%,%prefix2%" -o2 "%MODELROOT%/"
+)
+
+rmdir /s/q temp
+
+echo:
+IF ERRORLEVEL 1 (
+    echo ModelCompiler failed!
+) ELSE (
+    echo ModelCompiler succeeded!
+)
---
+++ src/PluginNodes/Boiler2PluginNodes.cs
@@ -0,0 +1,73 @@
+﻿namespace OpcPlc.PluginNodes;
+
+using BoilerModel2;
+using Opc.Ua;
+using OpcPlc.PluginNodes.Models;
+using System.Collections.Generic;
+using System.Reflection;
+
+/// <summary>
+/// Boiler node that inherits from DI.
+/// </summary>
+public class Boiler2PluginNodes : IPluginNodes
+{
+    public IReadOnlyCollection<NodeWithIntervals> Nodes { get; private set; } = new List<NodeWithIntervals>();
+
+    private static bool _isEnabled;
+    private PlcNodeManager _plcNodeManager;
+    private BoilerState _node;
+
+    public void AddOptions(Mono.Options.OptionSet optionSet)
+    {
+        _isEnabled = true;
+    }
+
+    public void AddToAddressSpace(FolderState telemetryFolder, FolderState methodsFolder, PlcNodeManager plcNodeManager)
+    {
+        _plcNodeManager = plcNodeManager;
+
+        if (_isEnabled)
+        {
+            AddNodes(methodsFolder);
+        }
+    }
+
+    public void StartSimulation()
+    {
+        _plcNodeManager.LoadPredefinedNodes(LoadPredefinedNodes);
+    }
+
+    public void StopSimulation()
+    {
+    }
+
+    private void AddNodes(FolderState methodsFolder)
+    {
+        // Load complex types from binary uanodes file.
+        _plcNodeManager.LoadPredefinedNodes(LoadPredefinedNodes);
+
+        // Find the Boiler2 node that was created when the model was loaded.
+        var passiveNode = (BaseObjectState)_plcNodeManager.FindPredefinedNode(new NodeId(BoilerModel2.Objects.Boilers, _plcNodeManager.NamespaceIndexes[(int)NamespaceType.Boiler]), typeof(BaseObjectState));
+
+        // Convert to node that can be manipulated within the server.
+        _node = new BoilerState(null);
+        _node.Create(_plcNodeManager.SystemContext, passiveNode);
+
+        _plcNodeManager.AddPredefinedNode(_node);
+    }
+
+    /// <summary>
+    /// Loads a node set from a file or resource and adds them to the set of predefined nodes.
+    /// </summary>
+    private NodeStateCollection LoadPredefinedNodes(ISystemContext context)
+    {
+        var predefinedNodes = new NodeStateCollection();
+
+        predefinedNodes.LoadFromBinaryResource(context,
+            "Boilers/Boiler2/BoilerModel2.PredefinedNodes.uanodes", // CopyToOutputDirectory -> PreserveNewest.
+            typeof(PlcNodeManager).GetTypeInfo().Assembly,
+            updateTables: true);
+
+        return predefinedNodes;
+    }
+}
