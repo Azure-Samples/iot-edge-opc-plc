408c41cbb5888c55f549ec3750ce8639755f90f2
Luis Cantero <lcantero@microsoft.com>
2023-05-19 22:39:38 +0200

    Add parse helpers for float, double and int

--- src/Helpers/CliHelper.cs
+++ src/Helpers/CliHelper.cs
@@ -1,11 +1,13 @@
 ﻿namespace OpcPlc.Helpers;
 
 using Mono.Options;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 
-public class CliHelper
+public static class CliHelper
 {
     /// <summary>
     /// Helper to build a list of filenames out of a comma separated list of filenames (optional in double quotes).
@@ -65,5 +67,67 @@ public class CliHelper
             }
         }
         return fileNames;
+    }
+
+    /// <summary>
+    /// Parse float value from string, verify that it is within the specified range and
+    /// round it to the specified number of decimal digits.
+    /// </summary>
+    public static float ParseFloat(string input, float min, float max, string optionName, int digits)
+    {
+        if (float.TryParse(input, CultureInfo.InvariantCulture, out float value))
+        {
+            if (value >= min && value <= max)
+            {
+                return (float)Math.Round(value, digits);
+            }
+            else
+            {
+                throw new OptionException($"The {optionName} {input} is not within the range {min} to {max}.", optionName);
+            }
+        }
+
+        throw new OptionException($"The {optionName} {input} is not a valid double.", optionName);
+    }
+
+    /// <summary>
+    /// Parse double value from string, verify that it is within the specified range and
+    /// round it to the specified number of decimal digits.
+    /// </summary>
+    public static double ParseDouble(string input, double min, double max, string optionName, int digits)
+    {
+        if (double.TryParse(input, CultureInfo.InvariantCulture, out double value))
+        {
+            if (value >= min && value <= max)
+            {
+                return Math.Round(value, digits);
+            }
+            else
+            {
+                throw new OptionException($"The {optionName} {input} is not within the range {min} to {max}.", optionName);
+            }
+        }
+
+        throw new OptionException($"The {optionName} {input} is not a valid double.", optionName);
+    }
+
+    /// <summary>
+    /// Parse int value from string and verify that it is within the specified range.
+    /// </summary>
+    public static double ParseInt(string input, int min, int max, string optionName)
+    {
+        if (int.TryParse(input, out int value))
+        {
+            if (value >= min && value <= max)
+            {
+                return value;
+            }
+            else
+            {
+                throw new OptionException($"The {optionName} {input} is not within the range {min} to {max}.", optionName);
+            }
+        }
+
+        throw new OptionException($"The {optionName} {input} is not a valid int.", optionName);
     }
 }
