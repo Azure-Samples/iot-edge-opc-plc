c80f04177f5103e0d7075855ee17c7f3dc9f2fab
Luis Cantero <lcantero@microsoft.com>
2023-05-25 09:03:17 +0200

    Simplify di node manager

--- src/CompanionSpecs/DI/DiNodeManager.cs
+++ src/CompanionSpecs/DI/DiNodeManager.cs
@@ -2,7 +2,6 @@
 
 using Opc.Ua;
 using Opc.Ua.Server;
-using System.Collections.Generic;
 using System.Reflection;
 
 /// <summary>
@@ -27,14 +26,6 @@ public sealed class DiNodeManager : CustomNodeManager2
     }
 
     /// <summary>
-    /// Creates the NodeId for the specified node.
-    /// </summary>
-    public override NodeId New(ISystemContext context, NodeState node)
-    {
-        return node.NodeId;
-    }
-
-    /// <summary>
     /// Loads a node set from a file or resource and adds them to the set of predefined nodes.
     /// </summary>
     protected override NodeStateCollection LoadPredefinedNodes(ISystemContext context)
@@ -46,85 +37,5 @@ public sealed class DiNodeManager : CustomNodeManager2
             updateTables: true);
 
         return predefinedNodes;
-    }
-
-    /// <summary>
-    /// Does any initialization required before the address space can be used.
-    /// </summary>
-    /// <remarks>
-    /// The externalReferences is an out parameter that allows the node manager to link to nodes
-    /// in other node managers. For example, the 'Objects' node is managed by the CoreNodeManager and
-    /// should have a reference to the root folder node(s) exposed by this node manager.
-    /// </remarks>
-    public override void CreateAddressSpace(IDictionary<NodeId, IList<IReference>> externalReferences)
-    {
-        lock (Lock)
-        {
-            LoadPredefinedNodes(SystemContext, externalReferences);
-        }
-    }
-
-    /// <summary>
-    /// Frees any resources allocated for the address space.
-    /// </summary>
-    public override void DeleteAddressSpace()
-    {
-        lock (Lock)
-        {
-            base.DeleteAddressSpace();
-        }
-    }
-
-    /// <summary>
-    /// Returns a unique handle for the node.
-    /// </summary>
-    protected override NodeHandle GetManagerHandle(ServerSystemContext context, NodeId nodeId, IDictionary<NodeId, NodeState> cache)
-    {
-        lock (Lock)
-        {
-            // quickly exclude nodes that are not in the namespace.
-            if (!IsNodeIdInNamespace(nodeId))
-            {
-                return null;
-            }
-
-            // check for predefined nodes.
-            if (PredefinedNodes != null && PredefinedNodes.TryGetValue(nodeId, out NodeState node))
-            {
-                var handle = new NodeHandle
-                {
-                    NodeId = nodeId,
-                    Validated = true,
-                    Node = node,
-                };
-
-                return handle;
-            }
-
-            return null;
-        }
-    }
-
-    /// <summary>
-    /// Verifies that the specified node exists.
-    /// </summary>
-    protected override NodeState ValidateNode(
-        ServerSystemContext context,
-        NodeHandle handle,
-        IDictionary<NodeId, NodeState> cache)
-    {
-        // not valid if no root.
-        if (handle == null)
-        {
-            return null;
-        }
-
-        // check if previously validated.
-        if (handle.Validated)
-        {
-            return handle.Node;
-        }
-
-        return null;
     }
 }
