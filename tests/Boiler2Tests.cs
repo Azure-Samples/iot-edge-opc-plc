4f1de5489742867ef7d5020f0d605f253ced383c
Luis Cantero <lcantero@microsoft.com>
2023-05-25 16:15:13 +0200

    Clean up

--- tests/Boiler2Tests.cs
+++ tests/Boiler2Tests.cs
@@ -1,6 +1,5 @@
 namespace OpcPlc.Tests;
 
-using BoilerModel2;
 using FluentAssertions;
 using NUnit.Framework;
 using Opc.Ua;
@@ -25,7 +24,6 @@ public class Boiler2Tests : SimulatorTestsBase
     [TearDown]
     public new virtual void TearDown()
     {
-        ////TurnHeaterOn();
     }
 
     [TestCase]
@@ -73,88 +71,4 @@ public class Boiler2Tests : SimulatorTestsBase
         currentTemperatureDegrees.Should().Be(53f);
         heaterState.Should().BeFalse();
     }
-
-    ////[TestCase]
-    ////public void Heater_CanBeTurnedOff()
-    ////{
-    ////    // let heater run for a few seconds to make temperature rise
-    ////    FireTimersWithPeriod(FromSeconds(1), numberOfTimes: 1000);
-
-    ////    TurnHeaterOff();
-
-    ////    FireTimersWithPeriod(FromSeconds(1), numberOfTimes: 1000);
-
-    ////    BoilerDataType model = GetBoilerModel();
-
-    ////    BoilerHeaterStateType state = model.HeaterState;
-    ////    BoilerTemperatureType temperature = model.Temperature;
-    ////    int pressure = model.Pressure;
-
-    ////    state.Should().Be(BoilerHeaterStateType.Off, "heater should have been turned off");
-    ////    pressure.Should().BeGreaterThan(10_000, "pressure should start at 10k and get higher");
-
-    ////    temperature.Top.Should().Be(pressure - 100_005, "top is always 100,005 less than pressure. Pressure: {0}", pressure);
-    ////    temperature.Bottom.Should().Be(pressure - 100_000, "bottom is always 100,000 less than pressure. Pressure: {0}", pressure);
-    ////}
-
-    ////[TestCase]
-    ////public void Heater_WhenRunning_HasRisingPressure()
-    ////{
-    ////    int previousPressure = 0;
-    ////    for (int i = 0; i < 5; i++)
-    ////    {
-    ////        FireTimersWithPeriod(FromSeconds(1), numberOfTimes: 1000);
-    ////        BoilerDataType model = GetBoilerModel();
-    ////        int pressure = model.Pressure;
-
-    ////        pressure.Should().BeGreaterThan(previousPressure, "pressure should build when heater is on");
-    ////        previousPressure = pressure;
-    ////    }
-    ////}
-
-    ////[TestCase]
-    ////public void Heater_WhenStopped_HasFallingPressure()
-    ////{
-    ////    int previousPressure = 0;
-    ////    for (int i = 0; i < 10; i++)
-    ////    {
-    ////        FireTimersWithPeriod(FromSeconds(1), numberOfTimes: 1000);
-    ////        BoilerDataType model = GetBoilerModel();
-    ////        int pressure = model.Pressure;
-
-    ////        pressure.Should().BeGreaterThan(previousPressure, "pressure should build when heater is on");
-    ////        previousPressure = pressure;
-    ////    }
-
-    ////    TurnHeaterOff();
-
-    ////    for (int i = 0; i < 5; i++)
-    ////    {
-    ////        FireTimersWithPeriod(FromSeconds(1), numberOfTimes: 1000);
-    ////        BoilerDataType model = GetBoilerModel();
-    ////        int pressure = model.Pressure;
-
-    ////        pressure.Should().BeLessThan(previousPressure, "pressure should drop when heater is off");
-    ////        previousPressure = pressure;
-    ////    }
-    ////}
-
-    ////private void TurnHeaterOn()
-    ////{
-    ////    var methodNode = NodeId.Create("HeaterOn", OpcPlc.Namespaces.OpcPlcBoiler, Session.NamespaceUris);
-    ////    Session.Call(GetOpcPlcNodeId("Methods"), methodNode);
-    ////}
-
-    ////private void TurnHeaterOff()
-    ////{
-    ////    var methodNode = NodeId.Create("HeaterOff", OpcPlc.Namespaces.OpcPlcBoiler, Session.NamespaceUris);
-    ////    Session.Call(GetOpcPlcNodeId("Methods"), methodNode);
-    ////}
-
-    ////private BoilerDataType GetBoilerModel()
-    ////{
-    ////    var nodeId = NodeId.Create(BoilerModel2.Variables.Boiler1_BoilerStatus, OpcPlc.Namespaces.OpcPlcBoiler, Session.NamespaceUris);
-    ////    var value = Session.ReadValue(nodeId).Value;
-    ////    return value.Should().BeOfType<ExtensionObject>().Which.Body.Should().BeOfType<BoilerDataType>().Subject;
-    ////}
 }
