1a358588f9958ba8b94cb77f7fb777a3bc628151
Luis Cantero <lcantero@microsoft.com>
2023-05-27 00:15:15 +0200

    Add device health tests

--- tests/Boiler2Tests.cs
+++ tests/Boiler2Tests.cs
@@ -5,8 +5,6 @@ using NUnit.Framework;
 using Opc.Ua;
 using Opc.Ua.Client;
 using Opc.Ua.DI;
-using System;
-using System.Linq;
 using static System.TimeSpan;
 
 /// <summary>
@@ -16,7 +14,7 @@ using static System.TimeSpan;
 public class Boiler2Tests : SimulatorTestsBase
 {
     public Boiler2Tests() : base(new[] {
-        "--b2ts=10",
+        "--b2ts=5",
         "--b2bt=1",
         "--b2tt=123",
         "--b2mi=567",
@@ -35,7 +33,7 @@ public class Boiler2Tests : SimulatorTestsBase
     {
         var nodeId = NodeId.Create(BoilerModel2.Variables.Boilers_Boiler__2_ParameterSet_TemperatureChangeSpeed, OpcPlc.Namespaces.OpcPlcBoiler, Session.NamespaceUris);
         var tempSpeedDegreesPerSec = (float)Session.ReadValue(nodeId).Value;
-        tempSpeedDegreesPerSec.Should().Be(10);
+        tempSpeedDegreesPerSec.Should().Be(5);
 
         nodeId = NodeId.Create(BoilerModel2.Variables.Boilers_Boiler__2_ParameterSet_MaintenanceInterval, OpcPlc.Namespaces.OpcPlcBoiler, Session.NamespaceUris);
         var maintenanceIntervalSeconds = (uint)Session.ReadValue(nodeId).Value;
@@ -47,7 +45,7 @@ public class Boiler2Tests : SimulatorTestsBase
 
         nodeId = NodeId.Create(BoilerModel2.Variables.Boilers_Boiler__2_ParameterSet_OverheatedThresholdTemperature, OpcPlc.Namespaces.OpcPlcBoiler, Session.NamespaceUris);
         var _overheatThresholdDegrees = (float)Session.ReadValue(nodeId).Value;
-        _overheatThresholdDegrees.Should().Be(123.0f + 10.0f);
+        _overheatThresholdDegrees.Should().Be(123f + 10f);
     }
 
     [TestCase]
@@ -57,8 +55,8 @@ public class Boiler2Tests : SimulatorTestsBase
         var currentTemperatureDegrees = (float)Session.ReadValue(currentTemperatureNodeId).Value;
         currentTemperatureDegrees.Should().Be(1f);
 
-        // Temperature rises with heater on for the next 10 s starting at 1°, step 10°.
-        FireTimersWithPeriod(FromSeconds(1), numberOfTimes: 10);
+        // Temperature rises with heater on for the next 20 s starting at 1°, step 5°.
+        FireTimersWithPeriod(FromSeconds(1), numberOfTimes: 20);
 
         currentTemperatureDegrees = (float)Session.ReadValue(currentTemperatureNodeId).Value;
 
@@ -68,74 +66,94 @@ public class Boiler2Tests : SimulatorTestsBase
         currentTemperatureDegrees.Should().Be(101f);
         heaterState.Should().BeTrue();
 
-        // Temperature rises until 123°, then falls with heater off, step -10°.
-        FireTimersWithPeriod(FromSeconds(1), numberOfTimes: 10);
+        // Temperature rises until 123°, then falls with heater off, step -5°.
+        FireTimersWithPeriod(FromSeconds(1), numberOfTimes: 20);
 
         currentTemperatureDegrees = (float)Session.ReadValue(currentTemperatureNodeId).Value;
 
         heaterState = (bool)Session.ReadValue(heaterStateNodeId).Value;
 
-        currentTemperatureDegrees.Should().Be(53f);
+        currentTemperatureDegrees.Should().Be(48f);
         heaterState.Should().BeFalse();
     }
 
     [TestCase]
     public void TestDeviceHealth()
     {
-        // NORMAL: Base temperature <= temperature <= target temperature
+        // 1. NORMAL: Base temperature <= temperature <= target temperature
+
         var deviceHealthNodeId = NodeId.Create(BoilerModel2.Variables.Boilers_Boiler__2_DeviceHealth, OpcPlc.Namespaces.OpcPlcBoiler, Session.NamespaceUris);
         var deviceHealth = (DeviceHealthEnumeration)Session.ReadValue(deviceHealthNodeId).Value;
+
         deviceHealth.Should().Be(DeviceHealthEnumeration.NORMAL);
 
-        // MAINTENANCE_REQUIRED: Triggered by the maintenance interval
+        // 2. MAINTENANCE_REQUIRED: Triggered by the maintenance interval
 
         // Fast forward to trigger maintenance required.
-        FireTimersWithPeriod(FromSeconds(1), numberOfTimes: 567);
+        FireTimersWithPeriod(FromSeconds(567), numberOfTimes: 1);
 
         deviceHealth = (DeviceHealthEnumeration)Session.ReadValue(deviceHealthNodeId).Value;
+
         // TODO: Fix spec, bcs state is overwritten immediately!
-        ////deviceHealth.Should().Be(DeviceHealthEnumeration.MAINTENANCE_REQUIRED);
+        deviceHealth.Should().Be(DeviceHealthEnumeration.MAINTENANCE_REQUIRED);
 
-        // FAILURE: Temperature > overheated temperature
+        // 3. FAILURE: Temperature > overheated temperature
+
         var currentTemperatureNodeId = NodeId.Create(BoilerModel2.Variables.Boilers_Boiler__2_ParameterSet_CurrentTemperature, OpcPlc.Namespaces.OpcPlcBoiler, Session.NamespaceUris);
-        // TODO: Fix spec, cannot write to current temperature!
-        var statusCode = WriteValue(Session, currentTemperatureNodeId, 123f + 100f);
-        statusCode.Should().Be(StatusCodes.Good);
 
-        // Fast forward to trigger overheated.
-        FireTimersWithPeriod(FromSeconds(1), numberOfTimes: 1);
+        // Fast forward to trigger overheat, then cool down for 2 s.
+        FireTimersWithPeriod(FromSeconds(678), numberOfTimes: 1);
+        FireTimersWithPeriod(FromSeconds(1), numberOfTimes: 2);
+
         var currentTemperatureDegrees = (float)Session.ReadValue(currentTemperatureNodeId).Value;
 
         deviceHealth = (DeviceHealthEnumeration)Session.ReadValue(deviceHealthNodeId).Value;
+
+        currentTemperatureDegrees.Should().Be(133);
         deviceHealth.Should().Be(DeviceHealthEnumeration.FAILURE);
 
-        // CHECK_FUNCTION: Target temperature < Temperature < overheated temperature
-        //...
+        // 4. CHECK_FUNCTION: Target temperature < Temperature < overheated temperature
 
-        // OFF_SPEC: Temperature < base temperature or temperature > overheated temperature + 5
-        // ...
-    }
+        // Fast forward to trigger overheat, then cool down for 3 s.
+        FireTimersWithPeriod(FromSeconds(678), numberOfTimes: 1);
+        FireTimersWithPeriod(FromSeconds(1), numberOfTimes: 3);
 
-    private StatusCode WriteValue(Session session, NodeId nodeId, object value)
-    {
-        var nodesToWrite = new WriteValueCollection
-        {
-            new WriteValue
-            {
-                NodeId = nodeId,
-                AttributeId = Attributes.Value,
-                Value = new DataValue()
-                {
-                    Value = value,
-                    StatusCode = StatusCodes.Good,
-                    ServerTimestamp = DateTime.Now,
-                    SourceTimestamp = DateTime.Now,
-                },
-            },
-        };
+        currentTemperatureDegrees = (float)Session.ReadValue(currentTemperatureNodeId).Value;
 
-        _ = session.Write(new RequestHeader(), nodesToWrite, out var writeResults, out _);
+        deviceHealth = (DeviceHealthEnumeration)Session.ReadValue(deviceHealthNodeId).Value;
 
-        return writeResults.FirstOrDefault();
+        currentTemperatureDegrees.Should().Be(128);
+        deviceHealth.Should().Be(DeviceHealthEnumeration.CHECK_FUNCTION);
+
+        // 5. OFF_SPEC 1: Temperature < base temperature
+
+        // Cool down for 5 s.
+        FireTimersWithPeriod(FromSeconds(1), numberOfTimes: 5);
+
+        currentTemperatureDegrees = (float)Session.ReadValue(currentTemperatureNodeId).Value;
+
+        var baseTemperatureNodeId = NodeId.Create(BoilerModel2.Variables.Boilers_Boiler__2_ParameterSet_BaseTemperature, OpcPlc.Namespaces.OpcPlcBoiler, Session.NamespaceUris);
+        var statusCode = WriteValue(baseTemperatureNodeId, currentTemperatureDegrees + 10f);
+        statusCode.Should().Be(StatusCodes.Good);
+
+        // Fast forward 1 s to update the DeviceHealth.
+        FireTimersWithPeriod(FromSeconds(1), numberOfTimes: 1);
+
+        deviceHealth = (DeviceHealthEnumeration)Session.ReadValue(deviceHealthNodeId).Value;
+
+        // TODO: Simulation does not allow base temp > current temp due to Min/Max.
+        ////deviceHealth.Should().Be(DeviceHealthEnumeration.OFF_SPEC);
+
+        // 6. OFF_SPEC 2: Temperature > overheated temperature + 5
+
+        // Fast forward to trigger overheat.
+        FireTimersWithPeriod(FromSeconds(678), numberOfTimes: 1);
+
+        currentTemperatureDegrees = (float)Session.ReadValue(currentTemperatureNodeId).Value;
+
+        deviceHealth = (DeviceHealthEnumeration)Session.ReadValue(deviceHealthNodeId).Value;
+
+        currentTemperatureDegrees.Should().Be(143);
+        deviceHealth.Should().Be(DeviceHealthEnumeration.OFF_SPEC);
     }
 }
