b2ac95213e744ac17286c515daa30f12c0f9726a
Luis Cantero <lcantero@microsoft.com>
2023-12-28 22:58:23 +0100

    Update sample

--- samples/OpcPlcBase.cs
+++ samples/OpcPlcBase.cs
@@ -10,10 +10,13 @@ using System.Threading.Tasks;
 /// </summary>
 public class OpcPlcBase
 {
-    private static bool _isOpcPlcServerRunning;
-    private static string[]? _args;
-    private readonly int _port;
+    private const int MaxPortTries = 10;
+
+    private readonly OpcPlcServer _opcPlcServer;
     private readonly bool _endpointUrlOverridden;
+    private readonly Random _rnd = new(1234); // Seeded (deterministic) random number generator.
+
+    private bool _isOpcPlcServerRunning;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="OpcPlcBase"/> class.
@@ -21,19 +24,13 @@ public class OpcPlcBase
     /// </summary>
     public OpcPlcBase(string[] args, int port = 51234, string? endpointUriOverride = null)
     {
-        if (_isOpcPlcServerRunning)
+        if (_isOpcPlcServerRunning && _opcPlcServer is not null)
         {
-            if (OpcPlcEndpointUrl.Contains(port.ToString()))
-            {
-                // Already running at the same port.
-                return;
-            }
-
-            OpcPlc.Program.Stop();
+            // Already running.
+            return;
         }
 
-        _args = args;
-        _port = port;
+        _opcPlcServer = new OpcPlcServer();
 
         if (!string.IsNullOrEmpty(endpointUriOverride))
         {
@@ -42,7 +39,27 @@ public class OpcPlcBase
             return;
         }
 
-        OpcPlcEndpointUrl = StartOpcPlcServerAsync(CancellationToken.None).GetAwaiter().GetResult();
+        // Try to use the specified port, otherwise find a deterministic random port.
+        int usedPortRetries = MaxPortTries;
+        while (OpcPlcEndpointUrl == string.Empty)
+        {
+            try
+            {
+                OpcPlcEndpointUrl = StartOpcPlcServerAsync(args, port, CancellationToken.None).GetAwaiter().GetResult();
+                break;
+            }
+            catch (Exception)
+            {
+#pragma warning disable S2589 // Boolean expressions should not be gratuitous
+                if (--usedPortRetries == 0)
+                {
+                    throw;
+                }
+#pragma warning restore S2589 // Boolean expressions should not be gratuitous
+
+                port = GetRandomEphemeralPort();
+            }
+        }
     }
 
     /// <summary>
@@ -60,10 +77,10 @@ public class OpcPlcBase
             throw new InvalidOperationException("Cannot restart OPC PLC server when the endpoint URL is overridden.");
         }
 
-        return OpcPlc.Program.RestartAsync();
+        return _opcPlcServer.RestartAsync();
     }
 
-    private static async Task<string> WaitForServerUpAsync(Task serverTask, CancellationToken cancellationToken)
+    private async Task<string> WaitForServerUpAsync(Task serverTask, CancellationToken cancellationToken)
     {
         while (true)
         {
@@ -79,7 +96,7 @@ public class OpcPlcBase
                 throw new Exception("The OPC PLC server failed to start.");
             }
 
-            if (!OpcPlc.Program.Ready)
+            if (!_opcPlcServer.Ready)
             {
                 await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken).ConfigureAwait(false);
                 continue;
@@ -87,25 +104,31 @@ public class OpcPlcBase
 
             _isOpcPlcServerRunning = true;
 
-            return OpcPlc.Program.PlcServer.GetEndpoints()[0].EndpointUrl;
+            return _opcPlcServer.PlcServer.GetEndpoints()[0].EndpointUrl;
         }
     }
 
-    private Task<string> StartOpcPlcServerAsync(CancellationToken cancellationToken)
+    private Task<string> StartOpcPlcServerAsync(string[] args, int port, CancellationToken cancellationToken)
     {
         // Passed args override the following defaults.
         var serverTask = Task.Run(
-            async () => await OpcPlc.Program.StartAsync(
-                _args?.Concat(
+            async () => await _opcPlcServer.StartAsync(
+                args?.Concat(
                     new[]
                     {
                         "--autoaccept",
-                        $"--portnum={_port}",
+                        $"--portnum={port}",
                     }).ToArray(),
                 cancellationToken)
             .ConfigureAwait(false),
             cancellationToken);
 
         return WaitForServerUpAsync(serverTask, cancellationToken);
+    }
+
+    private int GetRandomEphemeralPort()
+    {
+        // Port range 49152–65535 (https://en.wikipedia.org/wiki/Ephemeral_port).
+        return _rnd.Next(49152, 65535);
     }
 }
