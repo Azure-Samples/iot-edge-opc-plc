317825286020b345af9e78c8fceebe75e0dea061
Bhupendra Naphade <bhnaphad@microsoft.com>
2024-03-19 09:41:17 +0100

    removed unused code

--- src/Extensions/CancellationTokenExtensions.cs
+++ src/Extensions/CancellationTokenExtensions.cs
@@ -17,44 +17,4 @@ public static class CancellationTokenExtensions
         cancellationToken.Register(s => ((TaskCompletionSource<bool>)s).SetResult(true), tcs);
         return tcs.Task;
     }
-
-    private static string ROLE_INSTANCE
-    {
-        get
-        {
-            return System.Environment.MachineName;
-        }
-    }
-
-    private static string SIMULATION_ID
-    {
-        get
-        {
-            return Environment.GetEnvironmentVariable("SIMULATION_ID");
-        }
-    }
-
-    private static string KUBERNETES_NODE
-    {
-        get
-        {
-            return Environment.GetEnvironmentVariable("KUBERNETES_NODE");
-        }
-    }
-
-    private static string CLUSTER_NAME
-    {
-        get
-        {
-            return Environment.GetEnvironmentVariable("DEPLOYMENT_NAME");
-        }
-    }
-
-    private static string BUILD_NUMBER
-    {
-        get
-        {
-            return Environment.GetEnvironmentVariable("BUILD_NUMBER");
-        }
-    }
 }
