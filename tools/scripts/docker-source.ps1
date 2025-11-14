99ed3a589e560a4459f4473a4d08637aea400a1a
Luis Cantero <lcantero@microsoft.com>
2025-11-14 08:44:39 +0100

    Update to .NET 10

--- tools/scripts/docker-source.ps1
+++ tools/scripts/docker-source.ps1
@@ -87,7 +87,7 @@ ENV PATH="${PATH}:/root/vsdbg/vsdbg"
     $platforms = @{
         "linux/arm/v7" = @{
             runtimeId = "linux-arm"
-            image = "mcr.microsoft.com/dotnet/runtime-deps:9.0-bookworm-slim"
+            image = "mcr.microsoft.com/dotnet/runtime-deps:10.0-bookworm-slim"
             platformTag = "linux-arm32v7"
             runtimeOnly = "RUN chmod +x $($assemblyName)"
             debugger = $installLinuxDebugger
@@ -95,7 +95,7 @@ ENV PATH="${PATH}:/root/vsdbg/vsdbg"
         }
         "linux/arm64" = @{
             runtimeId = "linux-arm64"
-            image = "mcr.microsoft.com/dotnet/runtime-deps:9.0-bookworm-slim"
+            image = "mcr.microsoft.com/dotnet/runtime-deps:10.0-bookworm-slim"
             platformTag = "linux-arm64v8"
             runtimeOnly = "RUN chmod +x $($assemblyName)"
             debugger = $null
@@ -103,7 +103,7 @@ ENV PATH="${PATH}:/root/vsdbg/vsdbg"
         }
         "linux/amd64" = @{
             runtimeId = "linux-x64"
-            image = "mcr.microsoft.com/dotnet/runtime-deps:9.0-bookworm-slim"
+            image = "mcr.microsoft.com/dotnet/runtime-deps:10.0-bookworm-slim"
             platformTag = "linux-amd64"
             runtimeOnly = "RUN chmod +x $($assemblyName)"
             debugger = $installLinuxDebugger
